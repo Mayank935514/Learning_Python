0a1b21060e7c193117f5272f18590b504f5e5ea0
Mayank <mayankgarg8439@gmail.com>
2026-05-01 07:08:31 +0530

    Input & Output Functions

---
+++ 02.IO/01.printFunction.py
@@ -0,0 +1,14 @@
+# Example-1
+print("Hello")
+print("Welcome","to","World")
+print("Hope you are fine")
+print()
+
+# Example-2
+## 'end' and 'sep' in print()
+print("Welcome", end=" ")
+print("to world")
+print("25","08","2020", sep="-")
+print(10,20,30, sep="+", end=" ")
+print(40,50)
+
---
+++ 02.IO/02.inputFunction.py
@@ -0,0 +1,12 @@
+# To get input from the user
+# It always gives string as an input
+
+# Example-1
+name = input("Enter your name: ")
+print("Welcome "+name)
+
+# Example-2
+x=int(input("Enter First Number: "))
+y=int(input("Enter Second Number: "))
+res=x+y
+print("Sum is: ",res)
